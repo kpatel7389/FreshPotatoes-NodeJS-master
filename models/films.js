156688c20bd152c20dfd1e01efdeb20b02c1b9a1
Kosha Patel <kpatel7389@gmail.com>
2017-11-01 12:27:06 -0500

    connection with database and synced to sqlite using sequelize.  query for all films and some tests pass

---
+++ models/films.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var films = sequelize.define('films', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    release_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    tagline: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    revenue: {
+      type: DataTypes.BIGINT,
+      allowNull: false
+    },
+    budget: {
+      type: DataTypes.BIGINT,
+      allowNull: false
+    },
+    runtime: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    original_language: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    genre_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {timestamps: false})
+  return films;
+};
